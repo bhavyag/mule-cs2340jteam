244462c478891fa3c31ba196f093693390c59efd
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-08 00:51:05 +0000

    moved main method to game controller, small refactor on gui

--- CS 2340 Mule/src/GameController.java
+++ CS 2340 Mule/src/GameController.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: craigrmccown
@@ -6,4 +8,19 @@
  * To change this template use File | Settings | File Templates.
  */
 public class GameController {
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    GUI_Title window = new GUI_Title();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
