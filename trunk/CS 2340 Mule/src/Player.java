29bee22055e0e71550a95ffd9cab5d186f204fb4
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-08 00:59:28 +0000

    added player instance variables and constructor

--- trunk/CS 2340 Mule/src/Player.java
+++ trunk/CS 2340 Mule/src/Player.java
@@ -6,4 +6,20 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Player {
+    private String name;
+    private Color color;
+    private Race race;
+
+    public static enum Color {
+        RED, YELLOW, GREEN, PURPLE
+    }
+    public static enum Race {
+        BUZZITE, UGAITE, BONZIOD, FLAPPER, HUMAN
+    }
+
+    public Player(String name, Color color, Race race) {
+        this.name = name;
+        this.color = color;
+        this.race = race;
+    }
 }
