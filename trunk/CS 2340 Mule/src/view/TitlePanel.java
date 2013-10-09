3546ad59e11cbf56357525335b0772e1808932d5
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-09 16:53:40 +0000

    added view package and file for start page GUI

---
+++ trunk/CS 2340 Mule/src/view/TitlePanel.java
@@ -0,0 +1,42 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+public class TitlePanel {
+    private JPanel titlePanel;
+    private JLabel titleLabel;
+    private JButton btnStart;
+
+    public TitlePanel() {
+        titleLabel = new JLabel("");
+        titlePanel = new JPanel();
+        btnStart = new JButton("");
+    }
+
+    private void initialize() {
+        titleLabel.setBounds(0, 0, 1000, 563);
+        titleLabel.setIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/mule-title-1000x563.png")));
+        titlePanel.setLayout(null);
+        titlePanel.add(btnStart);
+        titlePanel.add(titleLabel);
+
+        btnStart.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        btnStart.setSelectedIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/start-button-selected.png")));
+        btnStart.setRolloverSelectedIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/start-button-selected.png")));
+        btnStart.setRolloverIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/start-button-selected.png")));
+        btnStart.setBounds(375, 343, 250, 80);
+        btnStart.setForeground(Color.WHITE);
+        btnStart.setIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/start-button.png")));
+        btnStart.setPressedIcon(new ImageIcon(TitlePanel.class.getResource("/sprites/start-button-selected.png")));
+        btnStart.setOpaque(false);
+        btnStart.setContentAreaFilled(false);
+        btnStart.setBorderPainted(false);
+        btnStart.setFocusPainted(false);
+    }
+
+    public JPanel getPanel() {
+        return titlePanel;
+    }
+}
