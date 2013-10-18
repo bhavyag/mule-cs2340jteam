db0c609996e5cf387bf442fac605ab06b1abbdb9
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-18 16:24:20 +0000

    Work on Tile class

--- trunk/CS 2340 Mule/src/model/Player.java
+++ trunk/CS 2340 Mule/src/model/Player.java
@@ -1,6 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Player {
+    /**
+     * ENUM for player color
+     */
     public static enum Color {
         RED("red"),
         YELLOW("yellow"),
@@ -17,6 +22,10 @@ public class Player {
             return name;
         }
     }
+
+    /**
+     * ENUM for player race
+     */
     public static enum Race {
         BUZZITE("Buzzite"),
         UGAITE("Ugaite"),
@@ -43,32 +52,60 @@ public class Player {
     private String name;
     private Color color;
     private Race race;
+    private ArrayList<Tile> playerTiles;
 
+    /**
+     * CONSTRUCTOR for Players
+     */
     public Player() {
         totalPlayers ++;
         this.playerNum = totalPlayers;
     }
 
+    /**
+     * Get the total number of players
+     * @return the total number of players
+     */
     public static int getTotalPlayers() {
         return totalPlayers;
     }
 
+    /**
+     * Get this players number
+     * @return the players number
+     */
     public int getPlayerNum() {
         return playerNum;
     }
 
+    /**
+     * Set this players name
+     * @param name what to make the players name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Set this players color
+     * @param color what to make the players color
+     */
     public void setColor(int color) {
         this.color = colors[color];
     }
 
+    /**
+     * Set this players race
+     * @param race what to make this players race
+     */
     public void setRace(int race) {
         this.race = races[race];
     }
 
+    /**
+     *
+     * @return
+     */
     public String toString() {
         return "Player " + playerNum + " is a " + color + " " + race + " named " + name;
     }
