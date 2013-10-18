e1aff9c6b96cd66c23a2303ecedfdedb31e57c0f
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-18 04:50:26 +0000

    Started work on Board and tile classes

--- trunk/CS 2340 Mule/src/model/Board.java
+++ trunk/CS 2340 Mule/src/model/Board.java
@@ -1,20 +1,40 @@
 package model;
 
-public abstract class Board {
-    public static Board constructBoard(int boardType) {
-        Board board = null;
+public class Board
+{
+    private Tile[][] myMap;
+    private String mapName;
+    private final Tile[][] map1 = new Tile[9][5];
+    private final Tile[][] map2 = new Tile[9][5];
 
-        switch (boardType) {
+    /**
+     * CONSTRUCTOR for a Board
+     * @param boardType an int representing the map that the board holds
+     */
+    public Board(int boardType)
+    {
+        switch(boardType)
+        {
             case 0:
-                board = new Map1();
+                this.myMap = map1;
+                this.mapName = "map 1";
                 break;
             case 1:
-                board = new Map2();
+                this.myMap = map2;
+                this.mapName = "map 2";
                 break;
             default:
-                return null;
+                this.myMap = map1;
+                this.mapName = "map 1";
         }
+    }
 
-        return board;
+    /**
+     * METHOD toString
+     * @return the name of this boards map.
+     */
+    public String toString()
+    {
+        return this.mapName;
     }
 }
