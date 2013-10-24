f4a6201a02c85afe677815329476fedfccafff34
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 01:27:23 +0000

    added tile arrays to both map constructors

--- trunk/CS 2340 Mule/src/model/Map1.java
+++ trunk/CS 2340 Mule/src/model/Map1.java
@@ -1,6 +1,46 @@
 package model;
 
 public class Map1 extends Board {
+    protected Map1() {
+        this.map = new Tile[][] {
+                {
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINONE), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.RIVER), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINTHREE), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS)
+                },
+                {
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.MOUNTAINONE),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.RIVER), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINTHREE)
+                },
+                {
+                        new Tile(Tile.Type.MOUNTAINTHREE), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.TOWN), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINONE)
+                },
+                {
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.MOUNTAINTWO),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.RIVER), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINTWO), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS)
+                },
+                {
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINTWO), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.RIVER), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.PLAINS), new Tile(Tile.Type.PLAINS),
+                        new Tile(Tile.Type.MOUNTAINTWO)
+                }
+        };
+    }
+
     public String toString() {
         return "map 1";
     }
