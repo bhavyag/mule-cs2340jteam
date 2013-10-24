2b83a9985a8fc5a282fd0e68678a9b4dd72a6401
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 18:58:19 +0000

    javadocced util package

--- CS 2340 Mule/src/util/LimitTimer.java
+++ CS 2340 Mule/src/util/LimitTimer.java
@@ -4,11 +4,20 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * CLASS LIMITTIMER a swing timer that can be configured with a time limit
+ */
 public class LimitTimer {
     private Timer timer;
     private int limit;
     private int count;
 
+    /**
+     * CONSTRUCTOR creates the timer
+     * @param limit the number of ticks before the time runs out
+     * @param delay the time between ticks (ms)
+     * @param listener a listener listening for when the timer ticks
+     */
     public LimitTimer(int limit, int delay, ActionListener listener) {
         this.limit = limit;
         this.timer = new Timer(delay, listener);
@@ -22,27 +31,42 @@ public class LimitTimer {
         });
     }
 
-    public int getTimeLimit() {
-        return limit;
-    }
-
+    /**
+     * METHOD gives the number of elapsed ticks since last reset
+     *
+     * @return the number of ticks
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     * METHOD determines whether time has run out
+     *
+     * @return true if time has run out, false otherwise
+     */
     public boolean isOutOfTime() {
         return count >= limit;
     }
 
+    /**
+     * METHOD resets the timer
+     */
     public void reset() {
         count = 0;
         timer.restart();
     }
 
+    /**
+     * METHOD starts the timer
+     */
     public void start() {
         timer.start();
     }
 
+    /**
+     * METHOD stops the timer
+     */
     public void stop() {
         timer.stop();
     }
