05f6a6c3126e1852c48af98901ab76dfe26755e9
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 00:21:13 +0000

    refactored board methods, commented out replacable methods and variables

--- CS 2340 Mule/src/model/Board.java
+++ CS 2340 Mule/src/model/Board.java
@@ -4,50 +4,50 @@ import model.Tile.Type;
 
 public abstract class Board
 {
-    private Tile[][] myMap = new Tile[9][5];
-    private String mapName;
-    private final String[][] map1 = {{"P","P", "M1","P", "R","P", "M3","P","P"},
-    								 {"P", "M1","P","P", "R","P","P","P", "M3"},									
-							    	 {"M3","P","P","P", "T","P","P","P", "M1"},									
-							    	 {"P", "M2","P","P", "R","P", "M2","P","P"},									
-							    	 {"P","P", "M2","P", "R","P","P","P", "M2"}
-    								};
-    		
-    private final String[][] map2 = {{"P","P", "P","M1", "P","P", "P","P","M3"},
-			 						 {"R", "R","P","P", "P","M1","P","P", "P"},									
-							    	 {"P","R","R","R", "T","R","R","R", "P"},									
-							    	 {"P", "M2","P","P", "P","P", "M2","R","R"},									
-							    	 {"P","P", "P","P", "M1","P","P","P", "P"}
-									};
+    private Tile[][] map = new Tile[9][5];
+
+//    private final String[][] map1 = {{"P","P", "M1","P", "R","P", "M3","P","P"},
+//    								 {"P", "M1","P","P", "R","P","P","P", "M3"},
+//							    	 {"M3","P","P","P", "T","P","P","P", "M1"},
+//							    	 {"P", "M2","P","P", "R","P", "M2","P","P"},
+//							    	 {"P","P", "M2","P", "R","P","P","P", "M2"}
+//    								};
+//
+//    private final String[][] map2 = {{"P","P", "P","M1", "P","P", "P","P","M3"},
+//			 						 {"R", "R","P","P", "P","M1","P","P", "P"},
+//							    	 {"P","R","R","R", "T","R","R","R", "P"},
+//							    	 {"P", "M2","P","P", "P","P", "M2","R","R"},
+//							    	 {"P","P", "P","P", "M1","P","P","P", "P"}
+//									};
     
-    private void initializeMap(String[][] map) {
-    	for (int x = 0; x < map.length; x++) {
-    		for (int y = 0; y < map[x].length; y++) {
-    			
-    			switch(map[x][y])
-    			{
-    			case "P":
-    				myMap[x][y] = new Tile(Type.PLAINS);
-    				break;
-    			case "R":
-    				myMap[x][y] = new Tile(Type.RIVER);
-    				break;
-    			case "M1":
-    				myMap[x][y] = new Tile(Type.MOUNTAINONE);
-    				break;
-    			case "M2":
-    				myMap[x][y] = new Tile(Type.MOUNTAINTWO);
-    				break;
-    			case "M3":
-    				myMap[x][y] = new Tile(Type.MOUNTAINTHREE);
-    				break;
-    			case "T":
-    				myMap[x][y] = new Tile(Type.TOWN);
-    				break;
-    			}
-    		}
-    	}
-    }
+//    private void initializeMap(String[][] map) {
+//    	for (int x = 0; x < map.length; x++) {
+//    		for (int y = 0; y < map[x].length; y++) {
+//
+//    			switch(map[x][y])
+//    			{
+//    			case "P":
+//    				myMap[x][y] = new Tile(Type.PLAINS);
+//    				break;
+//    			case "R":
+//    				myMap[x][y] = new Tile(Type.RIVER);
+//    				break;
+//    			case "M1":
+//    				myMap[x][y] = new Tile(Type.MOUNTAINONE);
+//    				break;
+//    			case "M2":
+//    				myMap[x][y] = new Tile(Type.MOUNTAINTWO);
+//    				break;
+//    			case "M3":
+//    				myMap[x][y] = new Tile(Type.MOUNTAINTHREE);
+//    				break;
+//    			case "T":
+//    				myMap[x][y] = new Tile(Type.TOWN);
+//    				break;
+//    			}
+//    		}
+//    	}
+//    }
 
     /**
      * METHOD get this boards map
@@ -55,15 +55,12 @@ public abstract class Board
      */
     public Tile[][] getMap()
     {
-        return this.myMap;
+        return this.map;
     }
 
     /**
      * METHOD toString
      * @return the name of this boards map.
      */
-    public String toString()
-    {
-        return this.mapName;
-    }
+    public abstract String toString();
 }
--- CS 2340 Mule/src/model/Map1.java
+++ CS 2340 Mule/src/model/Map1.java
@@ -1,5 +1,7 @@
 package model;
 
 public class Map1 extends Board {
-    private Tile[][] myMap = new Tile[9][5];
+    public String toString() {
+        return "map 1";
+    }
 }
