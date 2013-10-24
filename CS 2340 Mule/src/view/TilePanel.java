5d3b71aae9220ba58e9d8e845098e73f230e14ee
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 02:26:58 +0000

    More work on TilePanel

--- CS 2340 Mule/src/view/TilePanel.java
+++ CS 2340 Mule/src/view/TilePanel.java
@@ -13,89 +13,152 @@ public class TilePanel extends JPanel {
         this.setLayout(null);
         this.setBounds(168, 0, 832, 400); 
         
-        //COLUMN ZERO
-        JLabel label00 = new JLabel("New label");
+      //COLUMN ZERO
+        JLabel label00 = new JLabel();
         label00.setBounds(92, 20, 72, 72);
         add(label00);
         
-        JLabel label10 = new JLabel("New label");
+        JLabel label10 = new JLabel();
         label10.setBounds(92, 92, 72, 72);
         add(label10);
         
-        JLabel label20 = new JLabel("New label");
+        JLabel label20 = new JLabel();
         label20.setBounds(92, 164, 72, 72);
         add(label20);
         
-        JLabel label30 = new JLabel("New label");
+        JLabel label30 = new JLabel();
         label30.setBounds(92, 236, 72, 72);
         add(label30);
         
-        JLabel label40 = new JLabel("New label");
+        JLabel label40 = new JLabel();
         label40.setBounds(92, 308, 72, 72);
         add(label40);
         
-        //COLUMN ONE
-        JLabel label01 = new JLabel("New label");
+      //COLUMN ONE
+        JLabel label01 = new JLabel();
         label01.setBounds(164, 20, 72, 72);
         add(label01);
         
-        JLabel label11 = new JLabel("New label");
+        JLabel label11 = new JLabel();
         label11.setBounds(164, 92, 72, 72);
         add(label11);
         
-        JLabel label21 = new JLabel("New label");
+        JLabel label21 = new JLabel();
         label21.setBounds(164, 164, 72, 72);
         add(label21);
         
-        JLabel label31 = new JLabel("New label");
+        JLabel label31 = new JLabel();
         label31.setBounds(164, 236, 72, 72);
         add(label31);
         
-        JLabel label41 = new JLabel("New label");
+        JLabel label41 = new JLabel();
         label41.setBounds(164, 308, 72, 72);
         add(label41);
         
-        //COLUMN TWO
-        JLabel label02 = new JLabel("New label");
+      //COLUMN TWO
+        JLabel label02 = new JLabel();
         label02.setBounds(236, 20, 72, 72);
         add(label02);
         
-        JLabel label12 = new JLabel("New label");
+        JLabel label12 = new JLabel();
         label12.setBounds(236, 92, 72, 72);
         add(label12);
         
-        JLabel label22 = new JLabel("New label");
+        JLabel label22 = new JLabel();
         label22.setBounds(236, 164, 72, 72);
         add(label22);
         
-        JLabel label32 = new JLabel("New label");
+        JLabel label32 = new JLabel();
         label32.setBounds(236, 236, 72, 72);
         add(label32);
         
-        JLabel label42 = new JLabel("New label");
+        JLabel label42 = new JLabel();
         label42.setBounds(236, 308, 72, 72);
         add(label42);
         
-        //COLUMN THREE
-        JLabel label03 = new JLabel("New label");
-        label03.setBounds(236, 20, 72, 72);
+      //COLUMN THREE
+        JLabel label03 = new JLabel();
+        label03.setBounds(308, 20, 72, 72);
         add(label03);
         
-        JLabel label13 = new JLabel("New label");
-        label13.setBounds(236, 92, 72, 72);
+        JLabel label13 = new JLabel();
+        label13.setBounds(308, 92, 72, 72);
         add(label13);
         
-        JLabel label23 = new JLabel("New label");
-        label23.setBounds(236, 164, 72, 72);
+        JLabel label23 = new JLabel();
+        label23.setBounds(308, 164, 72, 72);
         add(label23);
         
-        JLabel label33 = new JLabel("New label");
-        label33.setBounds(236, 236, 72, 72);
+        JLabel label33 = new JLabel();
+        label33.setBounds(308, 236, 72, 72);
         add(label33);
         
-        JLabel label43 = new JLabel("New label");
-        label43.setBounds(236, 308, 72, 72);
+        JLabel label43 = new JLabel();
+        label43.setBounds(308, 308, 72, 72);
         add(label43);
+        
+      //COLUMN FOUR
+        JLabel label04 = new JLabel();
+        label04.setBounds(380, 20, 72, 72);
+        add(label04);
+        
+        JLabel label14 = new JLabel();
+        label14.setBounds(380, 92, 72, 72);
+        add(label14);
+        
+        JLabel label24 = new JLabel();
+        label24.setBounds(380, 164, 72, 72);
+        add(label24);
+        
+        JLabel label34 = new JLabel();
+        label34.setBounds(380, 236, 72, 72);
+        add(label34);
+        
+        JLabel label44 = new JLabel();
+        label44.setBounds(380, 308, 72, 72);
+        add(label44);
+        
+      //COLUMN FIVE
+        JLabel label05 = new JLabel();
+        label05.setBounds(452, 20, 72, 72);
+        add(label05);
+        
+        JLabel label15 = new JLabel();
+        label15.setBounds(452, 92, 72, 72);
+        add(label15);
+        
+        JLabel label25 = new JLabel();
+        label25.setBounds(452, 164, 72, 72);
+        add(label25);
+        
+        JLabel label35 = new JLabel();
+        label35.setBounds(452, 236, 72, 72);
+        add(label35);
+        
+        JLabel label45 = new JLabel();
+        label45.setBounds(452, 308, 72, 72);
+        add(label45);
+        
+      //COLUMN SIX
+        JLabel label06 = new JLabel();
+        label06.setBounds(524, 20, 72, 72);
+        add(label06);
+        
+        JLabel label16 = new JLabel();
+        label16.setBounds(524, 92, 72, 72);
+        add(label16);
+        
+        JLabel label26 = new JLabel();
+        label26.setBounds(524, 164, 72, 72);
+        add(label26);
+        
+        JLabel label36 = new JLabel();
+        label36.setBounds(524, 236, 72, 72);
+        add(label36);
+        
+        JLabel label46 = new JLabel();
+        label46.setBounds(524, 308, 72, 72);
+        add(label46);
           
     }
 }
