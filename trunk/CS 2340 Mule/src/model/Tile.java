d3e3c33106ee63b029b73bcfd2e58d01df0810cc
craigrmccown <craigrmccown@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 01:39:50 +0000

    added sprite locations to tile type enum

--- trunk/CS 2340 Mule/src/model/Tile.java
+++ trunk/CS 2340 Mule/src/model/Tile.java
@@ -1,26 +1,34 @@
 package model;
 
+import java.net.URL;
+
 public class Tile
 {
     /**
      * ENUM for Tile type
      */
     public static enum Type {
-        MOUNTAINONE("mountain 1"),
-        MOUNTAINTWO("mountain 2"),
-        MOUNTAINTHREE("mountain 3"),
-        PLAINS("plains"),
-        RIVER("river"),
-        TOWN("town");
+        MOUNTAINONE("mountain 1", this.getClass().getResource("/sprites/tiles/Mountain-Tile.png")),
+        MOUNTAINTWO("mountain 2", this.getClass().getResource("/sprites/tiles/Mountain-2-Tile.png")),
+        MOUNTAINTHREE("mountain 3", this.getClass().getResource("/sprites/tiles/Mountain-3-Tile.png")),
+        PLAINS("plains", this.getClass().getResource("/sprites/tiles/Plains-Tile.png")),
+        RIVER("river", this.getClass().getResource("/sprites/tiles/River-Tile.png")),
+        TOWN("town", this.getClass().getResource("/sprites/tiles/Town-Center-Tile.png"));
 
         private String type;
+        private URL imgPath;
 
-        Type(String myType) {
+        Type(String myType, URL imgPath) {
             this.type = myType;
+            this.imgPath = imgPath;
         }
 
         public String toString() {
             return type;
+        }
+
+        public URL getImgPath() {
+            return imgPath;
         }
     }
 
