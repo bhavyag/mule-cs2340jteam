fab2341e2670bcc3e1aa9c73003eac7b0cb48391
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-24 13:03:08 +0000

    JAVADOC

--- trunk/CS 2340 Mule/src/model/Tile.java
+++ trunk/CS 2340 Mule/src/model/Tile.java
@@ -2,9 +2,18 @@ package model;
 
 import java.net.URL;
 
+/**
+ * CLASS Tile. For representing the data of each tile on the map.
+ * @author Chris
+ *
+ */
 public class Tile
 {
-
+	/**
+	 * ENUM for the tile's border color.
+	 * @author Chris
+	 *
+	 */
 	public static enum TileColor {
         RED("red",Tile.class.getResource("/sprites/tiles/Border-Red.png")),
         YELLOW("yellow",Tile.class.getResource("/sprites/tiles/Border-Yellow.png")),
@@ -76,30 +85,54 @@ public class Tile
     	this.tileColor = TileColor.DEFAULT;
     }
 
+    /**
+     * METHOD to set the owner of the tile to the player passed in
+     * @param p the new owner
+     */
     public void setOwner(Player p)
     {
     	this.owner = p;
     }
 
+    /**
+     * METHOD to get the owner of this tile
+     * @return the owner of this tile
+     */
     public Player getOwner()
     {
     	return owner;
     }
 
+    /**
+     * METHOD to tell whether the tile is owned or not
+     * @return boolean of whether the tile is owned by a player.
+     */
     public boolean isOwned() {
         return owner == null;
     }
     
+    /**
+     * METHOD to get the type of this tile
+     * @return the type of this tile
+     */
     public Type getType()
     {
     	return this.type;
     }
     
+    /**
+     * METHOD to get this tile's border color
+     * @return this tile's border color
+     */
     public TileColor getTileColor()
     {
     	return this.tileColor;
     }
     
+    /**
+     * METHOD to set this tile's border color
+     * @param tileColor this tile's new border color
+     */
     public void setTileColor(TileColor tileColor)
     {
     	this.tileColor = tileColor;
