521126a2a3a130d8f212947d439b860301cfc985
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-10-30 19:59:43 +0000

    added some methods we can use for moving the plaayer on the screen

--- CS 2340 Mule/src/view/game/TownCenterPanel.java
+++ CS 2340 Mule/src/view/game/TownCenterPanel.java
@@ -3,7 +3,12 @@ package view.game;
 import view.title.TitlePanel;
 
 import javax.swing.*;
+
 import java.awt.FlowLayout;
+import java.awt.Point;
+import java.net.URL;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
  * CLASS TownCenterPanel. Displays the Town Center
@@ -12,6 +17,7 @@ import java.awt.FlowLayout;
  */
 public class TownCenterPanel extends JPanel {
     private JLabel townCenterPanel;
+    private JLabel playerLabel;
 
     /**
      * CONSTRUCTOR for the Town Center, instantiates the town centers label.
@@ -22,6 +28,26 @@ public class TownCenterPanel extends JPanel {
         initialize();
     }
 
+    /**
+     * METHOD to set which player to display in the town center.
+     * @param imagePath the image of the player
+     */
+    protected void setPlayerImage(URL imagePath)
+    {
+    	playerLabel.setIcon(new ImageIcon(imagePath));
+    }
+    
+    /**
+     * METHOD to set the player's position on the screen.
+     * @param p the player's new position
+     */
+    protected void setPlayerCoords(Point p)
+    {
+    	int x = (int) p.getX();
+    	int y = (int) p.getY();
+    	playerLabel.setBounds(x,y,40,40);
+    }
+    
     /**
      * METHOD to set the initial state of the town center.
      */
