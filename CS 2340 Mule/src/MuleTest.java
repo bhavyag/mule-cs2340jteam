4a8904122f0fcc4af362dbecc2c2725458342ccc
bhavya4651@gmail.com <bhavya4651@gmail.com@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-11-19 09:10:43 +0000

    Junit tests

--- CS 2340 Mule/src/MuleTest.java
+++ CS 2340 Mule/src/MuleTest.java
@@ -11,7 +11,7 @@ public class MuleTest
 	public void test() {
 		fail("Not yet implemented");
 	}
-	
+
 	/*
 	 * Chris's Tests
 	 */
@@ -25,25 +25,55 @@ public class MuleTest
 		playerChris.setSmithore(0);
 		playerChris.setEnergy(0);
 		playerChris.setCrystite(0);
-		
+
 		//set store resources
 		Market.setMarketFood(10);
 		Market.setMarketSmithore(10);
 		Market.setMarketEnergy(10);
 		Market.setMarketCrystite(10);
-		
+
 		//buying when player has no money
 		marketPanel.getBuyFoodButton().doClick();
 		marketPanel.getBuySmithoreButton().doClick();
 		marketPanel.getBuyEnergyButton().doClick();
 		marketPanel.getBuyCrystiteButton().doClick();
 		marketPanel.getTradeButton().doClick();
-		
-		
+
+
 		//buying when there's nothing to buy
-		
+
 	}
+
+
+
+
+
+
+
+	/*
+	 * Bhavya's Tests:
+	 * 
+	 * buying land
+	 * random events
+	 */
+
+	PlayerQueue playerQ2 = new PlayerQueue(1, 600);
+	
+	public void initializeTestsBhavya()
+	{
+		playerQ2.getCurrentPlayer().setFood(30);
+		playerQ2.getCurrentPlayer().setEnergy(30);
+		playerQ2.getCurrentPlayer().setSmithore(30);
+		playerQ2.getCurrentPlayer().setCrystite(30);
 		
-		
+		//call tests after this
+	}
+
+	private String testRandomEvents()
+	{
+		return RandomEvent.performRandom(playerQ2, playerQ2.getCurrentIndex());
+	}
+	
+	//player collision checking with edge of screen:
 
 }
