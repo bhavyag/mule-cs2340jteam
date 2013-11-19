4b326014d26a36436fcf092d5f2d6d5495d5629f
cheapeone <cheapeone@336882a7-10bc-d7cf-b5c9-92418daed82d>
2013-11-19 06:35:30 +0000

    fixed the junit file

---
+++ CS 2340 Mule/src/MuleTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MuleTest 
+{
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	/*
+	 * Chris's Tests
+	 */
+	
+	//test buy market methods
+		//make new player
+		//set player resources
+		//set store resources 
+		//buy a bunch of stuff
+		
+
+}
